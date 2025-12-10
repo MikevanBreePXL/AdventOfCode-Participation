a39b39782eb0a05b57a0539d776ffa082d1f3b0e
MikevanBreePXL <mike.vanbree@student.pxl.be>
2025-12-10 12:28:52 +0100

    Add D3C1 files to D3C2

---
+++ day-3/challenge-1/Main.py
@@ -0,0 +1,8 @@
+from Joltage_calculator import Joltage_Calculator
+
+def main():
+    calculator = Joltage_Calculator()
+    calculator.calculate_voltages()
+
+if __name__ == "__main__":
+    main()
